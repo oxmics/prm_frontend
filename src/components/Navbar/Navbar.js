b4076dfa62c0eccfece69a5db5b7d9dd9487dd48
Roshan R <roshanrajeev2001@gmail.com>
2022-11-18 17:12:13 +0530

    Navbar Dropdown

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import styles from "./Navbar.module.scss";
 
 const Navbar = ({ title }) => {
+    const [buttonExpand, setButtonExpand] = useState(false)
+
     const handleButtonClick = (e) => {
-        console.log("clicked")
+        setButtonExpand(!buttonExpand)
     }
+
+    useEffect(() => {
+        // const func = (e) => {
+        //     if (!([styles.Navbar__button, styles.Navbar__dropdown, styles.Navbar__dropdownBtn].includes(e.target.className))) {
+        //         setButtonExpand(false)
+        //     }
+        // }
+
+        // document.addEventListener('click', func)
+        // return () => {
+        //     document.removeEventListener('click', func)
+        // }
+    }, [])
 
     return (
         <div className={styles.Navbar}>
@@ -24,8 +39,16 @@ const Navbar = ({ title }) => {
                 <button className={styles.Navbar__button} onClick={handleButtonClick}>
                     <i className="bi bi-person"></i>
                     <span>Roshan</span>
+                    &nbsp;&nbsp;
+                    <i class="bi bi-chevron-down"></i>
                 </button>
             </div>
+            {buttonExpand &&
+                <ul className={styles.Navbar__dropdown}>
+                    <li><button className={styles.Navbar__dropdownBtn}>Profile</button></li>
+                    <li><button className={styles.Navbar__dropdownBtn}>Logout</button></li>
+                </ul>
+            }
         </div>
     )
 }
